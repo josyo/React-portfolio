283cb9f2ac7465b7f045995cf13ad82924b513f8
Joseph <jolakunle50@gmail.com>
2026-06-02 22:49:39 +0100

    Removed initial project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,3 @@
 export default function Projects() {
-    return (
-        <section 
-            id="projects"
-            className="py-16 sm:py-20 px-10 sm:px-6 lg:px-8 relative"
-        >
-            <div max-w->
-                <div className="text-center am:12 sm:mb-6 lg:mb-20">
-                    <h2 className="text-5xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">
-                        <span className="bg-gradient-to-b from-white to-gray-300 bg-clip-text text-transparent"
-                        >
-                            Your Complete Development
-                        </span>
-                        <br />
-                        <span 
-                        className="bg-gradient-to-b from-blue-400 to-cyan-400 bg-clip-text text-transparent"
-                        >
-                            Workflow
-                        </span>
-                    </h2>
-                </div>
-            </div>
-        </section>
-    )
+    return null
 }
